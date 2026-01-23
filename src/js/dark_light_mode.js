ede3e80d63f43ee7584acaf834384b910812a4c7
GUS MUNGUIA <gusmunguia@GUSs-MacBook-Pro.local>
2026-01-23 17:16:21 -0600

    Add text on web page: First sprint

--- src/js/dark_light_mode.js
+++ src/js/dark_light_mode.js
@@ -6,7 +6,7 @@
 const themeToggleBtn = document.getElementById('theme-toggle');
  themeToggleBtn.textContent = "light_mode"
 
-// Toggle logic
+// Toggle logic para cambiar el tema de la pagina web
 themeToggleBtn.addEventListener('click', () => {
   document.documentElement.classList.toggle('dark');
   
@@ -22,10 +22,14 @@ themeToggleBtn.addEventListener('click', () => {
 });
 
 
+//Desplaza al form de contacto con una animacion suave
 const buttonContactUs = document.getElementById('btnContactUs');
 const secContactUs = document.getElementById('secContactUs')
 buttonContactUs.addEventListener('click',()=>{
 
  secContactUs.scrollIntoView()
 
+
 })
+
+
