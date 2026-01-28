f24afc2b5631458e30cd4e062bfd1ceaf5335da6
SistemasGrupoMAFA <sistemas@grupomafa.com>
2026-01-28 13:56:09 -0600

    Feature/slide banners (#1)
    * Add new scale animations on solutions and industries section
    
    * Add text on web page: First sprint
    
    * Add carrousel number 1 at headers bottom
    
    * Add banners header carousel
    
    ---------
    
    Co-authored-by: GUS MUNGUIA <gusmunguia@GUSs-MacBook-Pro.local>

--- src/js/dark_light_mode.js
+++ src/js/dark_light_mode.js
@@ -1,8 +1,12 @@
+
+
+
+
 // This function change from dark theme to light theme
 const themeToggleBtn = document.getElementById('theme-toggle');
  themeToggleBtn.textContent = "light_mode"
 
-// Toggle logic
+// Toggle logic para cambiar el tema de la pagina web
 themeToggleBtn.addEventListener('click', () => {
   document.documentElement.classList.toggle('dark');
   
@@ -16,3 +20,16 @@ themeToggleBtn.addEventListener('click', () => {
     themeToggleBtn.textContent = "dark_mode"
   }
 });
+
+
+//Desplaza al form de contacto con una animacion suave
+const buttonContactUs = document.getElementById('btnContactUs');
+const secContactUs = document.getElementById('secContactUs')
+buttonContactUs.addEventListener('click',()=>{
+
+ secContactUs.scrollIntoView()
+
+
+})
+
+
