56532206f64067b5fb93b992aa2f3af6b55f3b22
GUS MUNGUIA <gusmunguia@GUSs-MacBook-Pro.local>
2026-01-28 14:47:57 -0600

    Remove dark theme fully

--- src/js/dark_light_mode.js
+++ src/js/dark_light_mode.js
@@ -1,27 +1,6 @@
 
 
 
-
-// This function change from dark theme to light theme
-const themeToggleBtn = document.getElementById('theme-toggle');
- themeToggleBtn.textContent = "light_mode"
-
-// Toggle logic para cambiar el tema de la pagina web
-themeToggleBtn.addEventListener('click', () => {
-  document.documentElement.classList.toggle('dark');
-  
-  if (document.documentElement.classList.contains('dark')) {
-    localStorage.removeItem("theme");
-    localStorage.theme = 'dark';
-    themeToggleBtn.textContent = "light_mode"
-   
-  } else {
-    localStorage.theme = 'light';
-    themeToggleBtn.textContent = "dark_mode"
-  }
-});
-
-
 //Desplaza al form de contacto con una animacion suave
 const buttonContactUs = document.getElementById('btnContactUs');
 const secContactUs = document.getElementById('secContactUs')
