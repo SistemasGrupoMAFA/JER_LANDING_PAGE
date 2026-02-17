9364a826ced279d69d855a6739ba77d9748c06ef
SistemasGrupoMAFA <sistemas@grupomafa.com>
2026-02-17 15:09:08 -0600

    Feature/align text (#9)
    * Add cambios colors, typography, animations from second deadline
    
    * Add new designs and first pagination trial
    
    * Add first pagination update
    
    * Add betta v1 pagination
    
    * Add alignment text on gallery

--- src/js/menuDeplegable.js
+++ src/js/menuDeplegable.js
@@ -1,6 +1,28 @@
  function toggleDropdown(menuId) {
-            const dropdownMenu = document
-            .getElementById(menuId);
-            dropdownMenu.classList.toggle('hidden');
+            const dropdownMenu = document.getElementById(menuId);
         
-        }
+           
+
+
+                
+                dropdownMenu.classList.toggle('hidden');
+                
+       
+ }
+
+
+
+    // const close = document.getElementById('inicioAnchor');
+    // const dropdownMenu = document.getElementById('dropdownMenuRight');
+    // close.addEventListener('click',()=>{
+
+
+       
+    //     dropdownMenu.classList.add('hidden');
+            
+
+
+
+    // })
+
+
